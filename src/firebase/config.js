ce8ed16c7f47b298a8ddad5c069ffa54d16a6fd2
Edwin Mwangi <edwinmwangi138@gmail.com>
2022-06-05 19:23:44 +0300

    firebase config and auth

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import firebase from '@firebase/app-compat';
+import 'firebase/compat/firestore'
+import 'firebase/auth'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMmZGJLJ07xSsD_jY5PMxcaqV5qQBVDfA",
+  authDomain: "data-blog-ac040.firebaseapp.com",
+  projectId: "data-blog-ac040",
+  storageBucket: "data-blog-ac040.appspot.com",
+  messagingSenderId: "988472332222",
+  appId: "1:988472332222:web:9e6d1a11cd3ce8979433ef"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp}
